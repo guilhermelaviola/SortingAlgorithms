8e30ad99aa4bcb47b8d80f5d6df5146d3c3de178
guilherme <guilhermelaviola96@gmail.com>
2023-03-30 15:59:30 -0300

    Added improved BubbleSort algorithm.

--- src/com/java/bubblesort/BubbleSort.java
+++ src/com/java/bubblesort/BubbleSort.java
@@ -5,34 +5,23 @@ public class BubbleSort {
 	// Testing BubbleSort
 	public static void main(String[] args) {
 		// Ordered arrays
-		int[] oa1 = {1, 2, 3}; 
-		int[] oa2 = {1, 2, 3, 4}; 
-		int[] oa3 = {1, 2, 3, 4, 5}; 
-		int[] oa4 = {1, 2, 3, 4, 5, 6}; 
-		int[] oa5 = {1, 2, 3, 4, 5, 6, 7}; 
-		int[] oa6 = {1, 2, 3, 4, 5, 6, 7, 8}; 
-		int[] oa7 = {1, 2, 3, 4, 5, 6, 7, 8, 9}; 
-		int[] oa8 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
-		int[] oa9 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; 
-		int[] oa10 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; 
-
+		int[] oa1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; 
+		int[] oa2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; 
 		// Unordered arrays
-		int[] uoa1 = {17, 16, 15}; 
-		int[] uoa2 = {17, 16, 15, 14}; 
-		int[] uoa3 = {17, 16, 15, 14, 13}; 
-		int[] uoa4 = {17, 16, 15, 14, 13, 12}; 
-		int[] uoa5 = {17, 16, 15, 14, 13, 12, 11}; 
-		int[] uoa6 = {17, 16, 15, 14, 13, 12, 11, 10}; 
-		int[] uoa7 = {17, 16, 15, 14, 13, 12, 11, 10, 9}; 
-		int[] uoa8 = {17, 16, 15, 14, 13, 12, 11, 10, 9, 8}; 
-		int[] uoa9 = {17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7}; 
-		int[] uoa10 = {17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6}; 
-		
-		System.out.println("Displaying unordered array 5: ");
-		displayArray(uoa5);
+		int[] uoa1 = {17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6}; 
+		int[] uoa2 = {17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6}; 
+		// Displaying arrays
+		System.out.println("Displaying unordered arrays 1 and 2 (which are the same): ");
+		displayArray(uoa1);
+		displayArray(uoa2);	
 		System.out.println();
-		System.out.println("Displaying the values of unordered array 5, now ordered: ");
-		bubbleSort(uoa5);
+		// Testing BubbleSort
+		System.out.println("Displaying the values of unordered array 1, now ordered: ");
+		bubbleSort(uoa1);
+		System.out.println();
+		// Testing improved BubbleSort
+		System.out.println("Displaying the values of unordered array 2, now ordered (with improved BubbleSort): ");
+		improvedBubbleSort(uoa2, 0);
 	}
 	
 	// Displaying an array
@@ -47,6 +36,7 @@ public class BubbleSort {
 	
 	// BubbleSort algorithm to order an unordered array
 	public static void bubbleSort(int[] array) {
+		long start = System.currentTimeMillis();
 		for(int i = 0; i < array.length-1; i++) {
 			for(int j = 0; j < array.length-1; j++) {
 				if(array[j] > array[j+1]) {
@@ -54,8 +44,36 @@ public class BubbleSort {
 					array[j] = array[j+1];
 					array[j+1] = aux;
 				}
+				displayArray(array);			}
+		}
+		long end = System.currentTimeMillis();
+		long totalTime = end - start;
+		System.out.println("Total time spent: " + totalTime + " ms.");
+	}
+	
+	// Improved BubbleSort algorithm
+	public static int improvedBubbleSort(int[] array, int times) {
+		long start = System.currentTimeMillis();
+		for(int i = 0; i < array.length-1; i++) {
+			// Here is the improvement
+			boolean flag = false;
+			for(int j = 0; j < array.length-1-i; j++) {
+				// Counting how many times the process is repeated
+				times++;
+				if(array[j] > array[j+1]) {
+					int aux = array[j];
+					array[j] = array[j+1];
+					array[j+1] = aux;
+					flag = true;
+				}
 				displayArray(array);
 			}
+			if(flag == false)
+				return times;
 		}
+		long end = System.currentTimeMillis();
+		long totalTime = end - start;
+		System.out.println("Total time spent: " + totalTime + " ms.");
+		return times;
 	}
 }
